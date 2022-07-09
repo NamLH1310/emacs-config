7968470048ccfe93877cfeb1fba18ab4b1eb6a2e
Le Hoang Nam <namlh5@vng.com.vn>
2022-07-09 16:17:22 +0700

    Add meow mode and more plugins

--- init.el
+++ init.el
@@ -1,3 +1,94 @@
+(defun meow-setup ()
+  (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
+  ;; (meow-define-keys 'insert '("C-[" . meow-insert-exit))
+  (meow-motion-overwrite-define-key '("j" . meow-next)
+				    '("k" . meow-prev)
+				    '("<escape>" . ignore))
+  (meow-leader-define-key
+   ;; SPC j/k will run the original command in MOTION state.
+   '("k" . (lambda ()
+	     (interactive)
+	     (scroll-down)
+	     (move-to-window-line nil)))
+   '("j" . (lambda ()
+	     (interactive)
+	     (scroll-up)
+	     (move-to-window-line nil)))
+   ;; Use SPC (0-9) for digit arguments.
+   '("1" . meow-digit-argument)
+   '("2" . meow-digit-argument)
+   '("3" . meow-digit-argument)
+   '("4" . meow-digit-argument)
+   '("5" . meow-digit-argument)
+   '("6" . meow-digit-argument)
+   '("7" . meow-digit-argument)
+   '("8" . meow-digit-argument)
+   '("9" . meow-digit-argument)
+   '("0" . meow-digit-argument)
+   '("/" . meow-keypad-describe-key)
+   '("?" . meow-cheatsheet))
+  (meow-normal-define-key '("0" . meow-expand-0)
+			  '("9" . meow-expand-9)
+			  '("8" . meow-expand-8)
+			  '("7" . meow-expand-7)
+			  '("6" . meow-expand-6)
+			  '("5" . meow-expand-5)
+			  '("4" . meow-expand-4)
+			  '("3" . meow-expand-3)
+			  '("2" . meow-expand-2)
+			  '("1" . meow-expand-1)
+			  '("-" . negative-argument)
+			  '(";" . meow-reverse)
+			  '("," . meow-inner-of-thing)
+			  '("." . meow-bounds-of-thing)
+			  '("[" . meow-beginning-of-thing)
+			  '("]" . meow-end-of-thing)
+			  '("a" . meow-append)
+			  '("A" . meow-open-below)
+			  '("b" . meow-back-word)
+			  '("B" . meow-back-symbol)
+			  '("c" . meow-change)
+			  '("d" . meow-delete)
+			  '("D" . meow-backward-delete)
+			  '("e" . meow-next-word)
+			  '("E" . meow-next-symbol)
+			  '("f" . meow-find)
+			  '("g" . meow-cancel-selection)
+			  '("G" . meow-grab)
+			  '("h" . meow-left)
+			  '("H" . meow-left-expand)
+			  '("i" . meow-insert)
+			  '("I" . meow-open-above)
+			  '("j" . meow-next)
+			  '("J" . meow-next-expand)
+			  '("k" . meow-prev)
+			  '("K" . meow-prev-expand)
+			  '("l" . meow-right)
+			  '("L" . meow-right-expand)
+			  '("m" . meow-join)
+			  '("n" . meow-search)
+			  '("o" . meow-block)
+			  '("O" . meow-to-block)
+			  '("p" . meow-yank)
+			  '("q" . meow-quit)
+			  '("Q" . meow-goto-line)
+			  '("r" . meow-replace)
+			  '("R" . meow-swap-grab)
+			  '("s" . meow-kill)
+			  '("t" . meow-till)
+			  '("u" . meow-undo)
+			  '("U" . meow-undo-in-selection)
+			  '("v" . meow-visit)
+			  '("w" . meow-mark-word)
+			  '("W" . meow-mark-symbol)
+			  '("x" . meow-line)
+			  '("X" . meow-goto-line)
+			  '("y" . meow-save)
+			  '("Y" . meow-sync-grab)
+			  '("z" . meow-pop-selection)
+			  '("'" . repeat)
+			  '("<escape>" . ignore)))
+
 (defun my-backup-file-name (fpath)
   "Return a new file path of a given file path. If the new path's directories does not exist, create them."
   (let* (
@@ -6,6 +97,14 @@
         (backupFilePath (replace-regexp-in-string "//" "/" (concat backupRootDir filePath "~") )))
     (make-directory (file-name-directory backupFilePath) (file-name-directory backupFilePath))
     backupFilePath))
+
+(defun newline-under-cursor ()
+  "1. move to end of the line.
+  2. insert newline with index"
+  (interactive)
+  (let ((oldpos (point)))
+    (end-of-line)
+    (newline-and-indent)))
 
 (defvar bootstrap-version)
 (let ((bootstrap-file
@@ -24,37 +123,44 @@
 
 (use-package
   emacs
-  :init (set-face-attribute 'default nil
-			    :family "DejaVu Sans Mono"
-			    :height 140)
+  :init (cond ((eq system-type 'darwin)
+               ;; MacOS-specific code goes here.
+	       (set-face-attribute 'default nil :height 150))
+	      ((eq system-type 'gnu/linux)
+               ;; Linux-specific code goes here.
+	       (set-face-attribute 'default nil
+				   :family "DejaVu Sans Mono"
+				   :height 140)))
   (tool-bar-mode -1)
   (scroll-bar-mode -1)
   (electric-pair-mode)
+  (hl-line-mode)
   (setq inhibit-startup-screen t gc-cons-threshold 100000000 straight-use-package-by-default t
 	make-backup-file-name-function 'my-backup-file-name enable-recursive-minibuffers t
-	next-line-add-newlines t visible-bell nil custom-safe-themes t ring-bell-function 'ignore)
-  :config (global-set-key (kbd "C-`")
-			  (lambda ()
-			    (interactive)
-			    (switch-to-buffer nil))))
+	next-line-add-newlines t visible-bell nil custom-safe-themes t ring-bell-function 'ignore))
 
-(use-package doom-modeline
-  :config
-  (doom-modeline-mode))
+(use-package
+  general
+  :config (general-define-key "<C-return>" 'newline-under-cursor))
 
-(use-package rainbow-delimiters
-  :hook
-  (emacs-lisp-mode . rainbow-delimiters-mode))
+(use-package
+    doom-modeline
+  :config (doom-modeline-mode))
 
-(use-package which-key
-  :config
-  (which-key-mode))
+(use-package
+    rainbow-delimiters
+  :hook (emacs-lisp-mode . rainbow-delimiters-mode))
 
-(use-package all-the-icons)
+(use-package 
+  which-key 
+  :config (which-key-mode))
 
-(use-package move-text
-  :config
-  (move-text-default-bindings))
+(use-package
+  all-the-icons)
+
+(use-package
+  move-text
+  :config (move-text-default-bindings))
 
 (use-package
   company
@@ -62,7 +168,8 @@
 
 (use-package
   projectile
-  :config (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  :after general
+  :config (general-def projectile-mode-map "C-c p" 'projectile-command-map)
   (projectile-mode))
 
 (use-package
@@ -70,7 +177,8 @@
   :config (yas-reload-all)
   :hook (prog-mode . yas-minor-mode))
 
-(use-package all-the-icons-dired
+(use-package
+  all-the-icons-dired
   :hook (dired-mode . all-the-icons-dired-mode))
 
 (use-package org-bullets
@@ -144,44 +252,79 @@
   :custom (lsp-metals-server-args '("-J-Dmetals.allow-multiline-string-formatting=off"))
   :hook (scala-mode . lsp))
 
-(use-package 
-  exec-path-from-shell 
-  :config (dolist (var '("JAVA_HOME")) 
-	    (add-to-list 'exec-path-from-shell-variables var)) 
-  (setq exec-path-from-shell-arguments nil) 
+(use-package
+  exec-path-from-shell
+  :config (dolist (var '("JAVA_HOME"))
+	    (add-to-list 'exec-path-from-shell-variables var))
+  (setq exec-path-from-shell-arguments nil)
   (exec-path-from-shell-initialize))
 
 (use-package
   counsel
+  :after
+  general
   :init
   (setq ivy-use-virtual-buffers t ivy-count-format "(%d/%d) ")
-  (global-set-key (kbd "C-s") 'isearch-forward)
-  (global-set-key (kbd "M-x") 'counsel-M-x)
-  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-  (global-set-key (kbd "M-y") 'counsel-yank-pop)
-  (global-set-key (kbd "C-h f") 'counsel-describe-function)
-  (global-set-key (kbd "C-h v") 'counsel-describe-variable)
-  (global-set-key (kbd "C-c l") 'counsel-find-library)
-  (global-set-key (kbd "C-c i") 'counsel-info-lookup-symbol)
-  (global-set-key (kbd "C-c u") 'counsel-unicode-char)
-  (global-set-key (kbd "C-c j") 'counsel-set-variable)
-  (global-set-key (kbd "C-x b") 'ivy-switch-buffer)
-  (global-set-key (kbd "C-c v") 'ivy-push-view)
-  (global-set-key (kbd "C-c V") 'ivy-pop-view)
-  (global-set-key (kbd "C-c c") 'counsel-compile)
-  (global-set-key (kbd "C-c g") 'counsel-git)
-  (global-set-key (kbd "C-c j") 'counsel-git-grep)
-  (global-set-key (kbd "C-c L") 'counsel-git-log)
-  (global-set-key (kbd "C-c k") 'counsel-rg)
-  (global-set-key (kbd "C-c n") 'counsel-fzf)
-  (global-set-key (kbd "C-x l") 'counsel-locate)
-  (global-set-key (kbd "C-c J") 'counsel-file-jump)
+  (general-define-key "M-x" 'counsel-M-x)
+  (general-define-key :prefix "C-x"
+		      "b" 'ivy-switch-buffer "C-f" 'counsel-find-file)
+  (general-define-key :prefix "C-c"
+		      "c" 'counsel-compile "g" 'counsel-git "u" 'counsel-unicode-char)
+  (general-define-key :prefix "C-h"
+		      "f" 'counsel-describe-function "v" 'counsel-describe-variable)
+  ;; (global-set-key (kbd "C-c l") 'counsel-find-library)
+  ;; (global-set-key (kbd "C-c i") 'counsel-info-lookup-symbol)
+  ;; (global-set-key (kbd "C-c j") 'counsel-set-variable)
+  ;; (global-set-key (kbd "C-c k") 'counsel-rg)
+  ;; (global-set-key (kbd "C-c n") 'counsel-fzf)
+  ;; (global-set-key (kbd "C-x l") 'counsel-locate)
+  ;; (global-set-key (kbd "C-c J") 'counsel-file-jump)
   (global-set-key (kbd "C-S-s") 'swiper-isearch)
   :config
-  (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+  (general-def minibuffer-local-map "C-r" 'counsel-minibuffer-history)
   (ivy-mode))
 
-(use-package general)
+(use-package amx
+  :config
+  (amx-mode))
+
+(use-package counsel-projectile
+  :config
+  (counsel-projectile-mode))
+
+(use-package go-mode
+  :init
+  (setq gofmt-command "goimports")
+  :hook
+  (go-mode . (lambda ()
+	       (lsp)
+	       (setq tab-width 4)
+	       (push '(">=" . ?≥) prettify-symbols-alist)
+	       (push '("<=" . ?≤) prettify-symbols-alist)
+	       (push '("!=" . ?≠) prettify-symbols-alist)
+	       (prettify-symbols-mode 1))))
+
+(use-package yaml-mode)
+
+(use-package web-mode
+  :hook
+  (web-mode . (lambda () (setq tab-width 2)))
+  :config
+  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode)))
+
+(use-package js2-mode
+  :hook (js-mode . (lambda () (setq js-indent-level 2) (js2-minor-mode))))
+
+(use-package meow
+  :config
+  (meow-setup)
+  (meow-global-mode))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
