babc165c9f0af8fbd20739e0bdd8cd291385d3ff
Lê Hoàng Nam <namlehoang15@gmail.com>
2022-07-10 22:24:43 +0700

    Add multi vterm

--- init.el
+++ init.el
@@ -330,6 +330,8 @@
   (meow-setup)
   (meow-global-mode))
 
+(use-package multi-vterm)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
